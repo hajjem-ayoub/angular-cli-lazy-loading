2b8905957e6396091034396762b1dc4cb6165cda
Ayoub <mah@trifork.com>
2017-01-27 11:01:34 +0100

    working lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { appRouting } from './app.routing';
+import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import "rxjs";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    appRouting,
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
